c330649aa46372df2366f61cd476675744abae75
Emeric Marès <emeric.mares@mines-paristech.fr>
2022-01-28 09:55:10 +0100

    début

--- Rogue.py
+++ Rogue.py
@@ -10,6 +10,7 @@ COTE = 20 # largeur du rectangle en pixels
 NB_CASES = 30
 screen = pg.display.set_mode((COTE*NB_CASES, COTE*NB_CASES))
 clock = pg.time.Clock()
+pace = 2
 
 
 WHITE = (255, 255, 255)
@@ -17,12 +18,31 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+wall = []
 
+def draw_rect(screen, x, y, size, color):
+    rect = pg.Rect(x*size, y*size, size, size)
+    pg.draw.rect(screen, color, rect)
+
+def room(pt, length, width):
+    pass
 
 running = True
 while running:
     clock.tick(pace)
 
+    for event in pg.event.get():
+        # chaque évênement à un type qui décrit la nature de l'évênement
+        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
+        if event.type == pg.QUIT:
+            running = False
+
+
+    
+
+
+    screen.fill(BLACK)
+    draw_rect(screen, 0, 1, COTE, WHITE)
 
     pg.display.update()
     
