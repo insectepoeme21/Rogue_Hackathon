fdf2b1914f2c23764a356a04d5c56c89a52ca13e
Max <lamarquemaxence@gmail.com>
2022-01-28 10:02:54 +0100

    ajout Knight classe et déplacement joueur

--- Rogue.py
+++ Rogue.py
@@ -5,6 +5,26 @@ import pygame as pg
 from random import randint
 from itertools import product
 
+
+KNIGHT_LIST=
+KNIGHT_POS={}
+
+class Knight:
+    def __init__(self, pos, HP=50, DMG=10, Ore = 10):
+        self.HP = HP
+        self.pos = pos
+        self.DMG = DMG
+        self.wealth = Ore
+
+def move(player, direction):
+    if player.pos + direction not in wall:
+        player.pos += direction
+    if player.pos + direction in KNIGHT_POS.keys():
+        fight(player, KNIGHT_POS[player.pos + direction])
+
+
+
+
 pg.init()
 COTE = 20 # largeur du rectangle en pixels
 NB_CASES = 30
@@ -22,6 +42,20 @@ BLUE = (0, 0, 255)
 running = True
 while running:
     clock.tick(pace)
+    
+    for event in pg.event.get():
+        if event.type == pg.K_z:
+            move(player, (0, 20))
+        if event.type == pg.K_s:
+            move(player, (0, -20))
+        if event.type == pg.K_q:
+            move(player, (-20, 0))
+        if event.type == pg.K_d:
+            move(player, (20, 0))
+
+
+        
+
 
 
     pg.display.update()
