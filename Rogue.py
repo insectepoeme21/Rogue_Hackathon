e4b6e66b3a1b2d583255e111336e3e5096cf1e60
Emeric Marès <emeric.mares@mines-paristech.fr>
2022-01-28 09:28:39 +0100

    skeleton

---
+++ Rogue.py
@@ -0,0 +1,30 @@
+# v1 : pareil mais au moins on peut sortir du programme
+# avec la touche 'q', ou avec la souris en fermant la fenêtre
+
+import pygame as pg
+from random import randint
+from itertools import product
+
+pg.init()
+COTE = 20 # largeur du rectangle en pixels
+NB_CASES = 30
+screen = pg.display.set_mode((COTE*NB_CASES, COTE*NB_CASES))
+clock = pg.time.Clock()
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+
+
+running = True
+while running:
+    clock.tick(pace)
+
+
+    pg.display.update()
+    
+
+pg.quit()
